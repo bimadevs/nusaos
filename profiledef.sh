0d6bb11d32bbd3283833469556d79ff21a32accd
bimadevs <bimadev06@gmail.com>
2026-07-05 13:37:40 +0700

    feat: configure DNS resolver, enable multilib repo, add build permissions
    Set Google DNS (8.8.8.8/8.8.4.4), enable multilib in pacman.conf, add customize_airootfs.sh execute permissions in profiledef.sh.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- profiledef.sh
+++ profiledef.sh
@@ -17,6 +17,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
+  ["/root/customize_airootfs.sh"]="0:0:755"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
